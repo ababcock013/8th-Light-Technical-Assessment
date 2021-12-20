ef841404d32ef1ff384b1fa53d32cb6feddae6ef
ababcock013@outlook.com <ababcock013@outlook.com>
2021-12-19 20:01:33 -0600

    Removed some deps

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
-import localStorage from "node-localStorage"
+
+
 import chalk from "chalk";
 import ora from "ora"
 
 import prompt from "prompt";
 import {retrieve} from './config/apiAccess.js'
 
+
 const getAll = async (query)=>{
+    const spinner = ora("Retrieving books...").start()
     await retrieve(encodeURIComponent(query)).then((data)=>{
-        console.log(data)
+        //console.log(data)
+        spinner.succeed("Books received!")
+        localStorage.setItem('books', data);
         }
     )
 }
@@ -18,6 +23,7 @@ prompt.start()
 console.log(" Enter a search term:")
 prompt.get('search', (err, result)=>{
     getAll(result.search)
+    console.log(localStorage.getItem('books'))
 })
 
 
