20ba017ee30fea143ff4e2081ad8f460ff2054d9
ababcock013 <ababcock013@outlook.com>
2021-12-20 01:46:21 -0600

    book localStorage setup

--- src/app.js
+++ src/app.js
@@ -1,30 +1,35 @@
-
+import enquirer from "enquirer";
+import boxen from "boxen";
+import { LocalStorage } from "node-localstorage";
+const localStorage = new LocalStorage("./scratch/");
 
 import chalk from "chalk";
-import ora from "ora"
+import ora from "ora";
 
 import prompt from "prompt";
-import {retrieve} from './config/apiAccess.js'
+import { retrieve } from "./config/apiAccess.js";
+import Enquirer from "enquirer";
 
 
-const getAll = async (query)=>{
-    const spinner = ora("Retrieving books...").start()
-    await retrieve(encodeURIComponent(query)).then((data)=>{
-        //console.log(data)
-        spinner.succeed("Books received!")
-        localStorage.setItem('books', data);
-        }
-    )
-}
+const getAll = async (query) => {
+  const spinner = ora("Retrieving books...").start();
+  await retrieve(encodeURIComponent(query)).then((data) => {
+    console.log(data);
+    spinner.succeed("Books received!");
+    localStorage.setItem(
+      "books.js",
+      JSON.stringify(data)
+        .replace(/^/, "const books =")
+        .concat(" \n export default books")
+    );
+    //console.log(localStorage.getItem('books'))
+  });
+};
 
-
-
-prompt.start()
-console.log(" Enter a search term:")
-prompt.get('search', (err, result)=>{
-    getAll(result.search)
-    console.log(localStorage.getItem('books'))
-})
+prompt.start();
+console.log('Welcome to Book Search')
+console.log(" Enter a search term to find a book:");
+prompt.get("search", (err, result) => { getAll(result.search) })
 
 
 
--- src/config/apiAccess.js
+++ src/config/apiAccess.js
@@ -1,33 +1,38 @@
-import axios from "axios"
-import {} from "dotenv/config"
-const URL = process.env.URL
-const KEY = process.env.G_BOOKS_API_KEY
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+
+const URL = "https://www.googleapis.com/books/v1/volumes?q=";
+const KEY = "&key=AIzaSyAnrofpaQau3mJIktgSO_K8wz2p11_-ecc";
 
 //Constrains query results to return only first 5 books
-const PARAMS = "&printType=books&startIndex=0&maxResults=5"
+const PARAMS = "&printType=books&startIndex=0&maxResults=5";
 
 export const retrieve = async (query, bookList = []) => {
+  let books = await axios
+    .get(`${URL}${query}${PARAMS}${KEY}`)
+    .then((res) => {
+      let bookNum = 1;
+      bookList = res.data.items.map((item) => {
+        return {
+          "Book Number": bookNum++,
+          title: item.volumeInfo.title,
+          authors: item.volumeInfo.authors,
+          publisher: item.volumeInfo.publisher,
+        };
 
-   let books = await axios.get(`${URL}/volumes?q=${query}${PARAMS}${KEY}`)
-        .then((res) => {
-            let bookNum = 1
-                bookList = res.data.items.map(item => {
-                    return {
-                        "Book Number": bookNum++,
-                        title:item.volumeInfo.title,
-                        authors:item.volumeInfo.authors,
-                        publisher:item.volumeInfo.publisher
-                    }
-                })
-            return bookList
-        })
-        .catch((e)=>{
-            if(e.type === undefined && query === ""){
-                console.log("Blank query, please provide a search term.")
-            }else if(e.type === undefined){
-                console.log("No results for term: " + decodeURIComponent(query))
-            }
-      })
-    return books
-}
+      });
 
+      return bookList;
+    })
+    .catch((e) => {
+      if (e.type === undefined && query === "") {
+        console.log("Blank query, please provide a search term.");
+      } else if (e.type === undefined) {
+        console.log("No results for term: " + decodeURIComponent(query));
+      }
+    });
+  return books;
+};
+
+//`${URL}/volumes?q=${query}${PARAMS}${KEY}`
